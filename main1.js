332e384479bd02b50c8dd3d1fa45baec4fc564e5
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-05-14 12:30:29 -0400

    Add files via upload

--- main1.js
+++ main1.js
@@ -36,6 +36,25 @@ function getResult(error, results){
         document.getElementById("result_emotion_name2").innerHTML = results[1].label;
         prediction1 = results[0].label;
         prediction2 = results[1].label; 
+        if(results[0].label == "Happy"){
+            document.getElementById("update_emoji").innerHTML = "&#128522";
+        }
+        else if(results[0].label == "Sad"){
+            document.getElementById("update_emoji").innerHTML = "&#128532";
+        }
+        else if(results[0].label == "Angry"){
+            document.getElementById("update_emoji").innerHTML = "&#128545"
+        }
+
+        if(results[1].label == "Happy"){
+            document.getElementById("update_emoji2").innerHTML = "&#128522";
+        }
+        else if(results[1].label == "Sad"){
+            document.getElementById("update_emoji2").innerHTML = "&#128532";
+        }
+        else if(results[1].label == "Angry"){
+            document.getElementById("update_emoji2").innerHTML = "&#128545"
+        }
         speak();
     }
 }
@@ -43,7 +62,7 @@ function getResult(error, results){
 function speak() {
     synth = window.speechSynthesis;
     speak1 = "The first prediction is " + prediction1;
-    speak2 = "and the prediction is  " + prediction2;
+    speak2 = " and the second prediction is  " + prediction2;
     utterThis = new SpeechSynthesisUtterance(speak1 + speak2);
     synth.speak(utterThis);
 }
